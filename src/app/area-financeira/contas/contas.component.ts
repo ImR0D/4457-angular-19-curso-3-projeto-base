d08f14d9c7108fb87c2a2231c6a53eb19ae1f540
Rodrigo Ferreira <rodrigo.ferreira@pca.com.br>
2026-06-22 11:18:10 -0300

    refactor: implementado projeção de conteúdo com ng-content no componente de botão para deixar o componente dinâmico e permitir customizações

--- src/app/area-financeira/contas/contas.component.ts
+++ src/app/area-financeira/contas/contas.component.ts
@@ -1,13 +1,13 @@
 import { Component, input } from '@angular/core';
-import { BotaoComponent } from "../../compartilhados/botao/botao.component";
-import { ContaComponent } from "./conta/conta.component";
+import { BotaoComponent } from '../../compartilhados/botao/botao.component';
+import { ContaComponent } from './conta/conta.component';
 import { Conta } from '../compartilhados/conta.model';
 
 @Component({
   selector: 'app-contas',
   imports: [BotaoComponent, ContaComponent],
   templateUrl: './contas.component.html',
-  styleUrl: './contas.component.css'
+  styleUrl: './contas.component.css',
 })
 export class ContasComponent {
   contas = input.required<Conta[]>();
