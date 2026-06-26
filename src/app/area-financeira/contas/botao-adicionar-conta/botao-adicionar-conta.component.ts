b2a28f64b670da44e98d9d9206aee002153cb386
Rodrigo Ferreira <rodrigo.ferreira@pca.com.br>
2026-06-26 10:12:53 -0300

    feat: implementado componente para adicionar contas com modal e removido propriedade de diretiva e classe não utilizada no componente de adicionar transacao

---
+++ src/app/area-financeira/contas/botao-adicionar-conta/botao-adicionar-conta.component.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
+import { ModalComponent } from '../../../compartilhados/modal/modal.component';
+
+@Component({
+  selector: 'app-botao-adicionar-conta',
+  imports: [BotaoComponent, ModalComponent],
+  templateUrl: './botao-adicionar-conta.component.html',
+  styleUrl: './botao-adicionar-conta.component.css',
+})
+export class BotaoAdicionarContaComponent {
+  openModal = signal(false);
+
+  openModalDialog() {
+    this.openModal.set(true);
+  }
+}
