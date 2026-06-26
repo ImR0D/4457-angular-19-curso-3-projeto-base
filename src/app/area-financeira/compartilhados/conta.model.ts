1ceed60a0b7efe9dfa4e8921ecb790819c538679
Rodrigo Ferreira <rodrigo.ferreira@pca.com.br>
2026-06-26 15:51:34 -0300

    feat: implementado lógica para formatação de nome e atualização de TipoBancos na criação de contas, ajustado botão de adicionar conta

--- src/app/area-financeira/compartilhados/conta.model.ts
+++ src/app/area-financeira/compartilhados/conta.model.ts
@@ -1,6 +1,17 @@
+import { TipoBancos } from './tiposBancos.type';
+
 export class Conta {
   constructor(
     public readonly nome: string,
     public readonly saldo: number,
-  ) {}
+  ) {
+    let _name =
+      nome.toLowerCase().charAt(0).toUpperCase() +
+      nome.substring(1, nome.length);
+
+    TipoBancos.update((tipos) => ({
+      ...tipos,
+      [_name]: nome,
+    }));
+  }
 }
