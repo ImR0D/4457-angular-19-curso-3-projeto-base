6270a8d6f623ce2f4f3de2e4cf63c70c77452f4d
Rodrigo Ferreira <rodrigo.ferreira@pca.com.br>
2026-06-26 11:44:21 -0300

    feat: implementado formulário para adicionar contas com validação e emissão de evento, ajustado layout e estilos

--- src/app/area-financeira/contas/botao-adicionar-conta/botao-adicionar-conta.component.ts
+++ src/app/area-financeira/contas/botao-adicionar-conta/botao-adicionar-conta.component.ts
@@ -1,17 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
 import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
 import { ModalComponent } from '../../../compartilhados/modal/modal.component';
+import { FormsModule } from '@angular/forms';
+import { Conta } from '../../compartilhados/conta.model';
 
 @Component({
   selector: 'app-botao-adicionar-conta',
-  imports: [BotaoComponent, ModalComponent],
+  imports: [BotaoComponent, ModalComponent, FormsModule],
   templateUrl: './botao-adicionar-conta.component.html',
   styleUrl: './botao-adicionar-conta.component.css',
 })
 export class BotaoAdicionarContaComponent {
   openModal = signal(false);
+  accountBank = output<Conta>();
+
+  objetoCriarConta = {
+    nome: '',
+    saldo: '',
+  };
 
   openModalDialog() {
     this.openModal.set(true);
+  }
+
+  submitAccount() {
+    const formAccount = new Conta(
+      this.objetoCriarConta.nome,
+      Number(this.objetoCriarConta.saldo),
+    );
+    this.accountBank.emit(formAccount);
+    this.openModal.set(false);
+    this.objetoCriarConta.nome = '';
+    this.objetoCriarConta.saldo = '';
   }
 }
